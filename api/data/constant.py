654c3bf35cafca2a12e339f7f294cba036a81505
Thomas MANCEAU <manceau.thomas19@gmail.com>
2023-11-28 16:18:12 +0000

    Merged in feature/LFEA-5-as-a-user-i-want-to-create-a-saving-account (pull request #2)
    feature/LFEA-5-as-a-user-i-want-to-create-a-saving-account
    
    * Add saving account
    
    
    Approved-by: Thomas MANCEAU

--- api/data/constant.py
+++ api/data/constant.py
@@ -4,11 +4,15 @@ from model.poco.account_type import AccountType
 # BANK ACCOUNT #
 ################
 
-SUPPORTED_BANK_ACCOUNT_TYPE = [ AccountType.STANDARD_ACCOUNT ]
+SUPPORTED_BANK_ACCOUNT_TYPE = [
+    AccountType.STANDARD_ACCOUNT,
+    AccountType.SAVING_ACCOUT
+]
 
 
 ###############
 # USER ERRORS #
 ###############
 
-USER_ERR_1 = 'Bank account type is not supported. Should be one of {}'.format(SUPPORTED_BANK_ACCOUNT_TYPE)
+USER_ERR_1 = 'Bank account type is not supported. Should be one of {}'.format(
+    SUPPORTED_BANK_ACCOUNT_TYPE)
